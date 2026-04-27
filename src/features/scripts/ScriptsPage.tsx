e03b099f9f9e94ad403404d06a97ab3db3cc7412
Emmanuel T. <contact@emmtej.com>
2026-04-27 12:36:25 +0200

    refactor(scripts): clean up ScriptsPage component

--- src/features/scripts/ScriptsPage.tsx
+++ src/features/scripts/ScriptsPage.tsx
@@ -1,133 +1,67 @@
 import { Box, Center, Flex, Loader } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect } from "react";
 import { useShallow } from "zustand/react/shallow";
-import type { Folder, ScriptSummary } from "@/features/storage/types";
 import { BreadcrumbNav } from "./components/BreadcrumbNav";
 import { MultiSelectToolbar } from "./components/MultiSelectToolbar";
 import { PreviewPanel } from "./components/PreviewPanel";
 import { ScriptsEmptyState } from "./components/ScriptsEmptyState";
 import { ScriptsHeader } from "./components/ScriptsHeader";
 import { ScriptsLibraryItems } from "./components/ScriptsLibraryItems";
-import { ScriptsModals } from "./components/ScriptsModals";
+import { ScriptsModals, useScriptsModalsStore } from "./components/ScriptsModals";
 import { useScriptsEvents } from "./hooks/useScriptsEvents";
 import { useScriptsUpload } from "./hooks/useScriptsUpload";
+import { useScriptsPageController } from "./hooks/useScriptsPageController";
 import { useScriptsDataStore } from "./store/useScriptsDataStore";
 import { useScriptsUiStore } from "./store/useScriptsUiStore";
 
 export default function ScriptsPage() {
-	const {
-		folders,
-		scripts,
-		breadcrumb,
-		folderChildItemCounts,
-		isLoading,
-		hasMoreScripts,
-		init,
-		fetchFolderData,
-		loadMoreScripts,
-		duplicateItems,
-	} = useScriptsDataStore(
-		useShallow((s) => ({
-			folders: s.folders,
-			scripts: s.scripts,
-			breadcrumb: s.breadcrumb,
-			folderChildItemCounts: s.folderChildItemCounts,
-			isLoading: s.isLoading,
-			hasMoreScripts: s.hasMoreScripts,
-			init: s.init,
-			fetchFolderData: s.fetchFolderData,
-			loadMoreScripts: s.loadMoreScripts,
-			duplicateItems: s.duplicateItems,
-		})),
-	);
+	const init = useScriptsDataStore((s) => s.init);
+	const isLoading = useScriptsDataStore((s) => s.isLoading);
+	const breadcrumb = useScriptsDataStore((s) => s.breadcrumb);
+	const hasMoreScripts = useScriptsDataStore((s) => s.hasMoreScripts);
+	const duplicateItems = useScriptsDataStore((s) => s.duplicateItems);
+	const scripts = useScriptsDataStore((s) => s.scripts);
 
 	const {
-		currentFolderId,
 		selectedScript,
 		selectedIds,
 		viewMode,
 		isPreviewLoading,
-		setCurrentFolder,
-		selectScript,
-		toggleSelection,
 		clearSelection,
 		setViewMode,
 	} = useScriptsUiStore(
 		useShallow((s) => ({
-			currentFolderId: s.currentFolderId,
 			selectedScript: s.selectedScript,
 			selectedIds: s.selectedIds,
 			viewMode: s.viewMode,
 			isPreviewLoading: s.isPreviewLoading,
-			setCurrentFolder: s.setCurrentFolder,
-			selectScript: s.selectScript,
-			toggleSelection: s.toggleSelection,
 			clearSelection: s.clearSelection,
 			setViewMode: s.setViewMode,
 		})),
 	);
 
+	const setCreateFolderOpened = useScriptsModalsStore((s) => s.setCreateFolderOpened);
+	const setMoveModalOpened = useScriptsModalsStore((s) => s.setMoveModalOpened);
+	const setDeleteItemsOpened = useScriptsModalsStore((s) => s.setDeleteItemsOpened);
+
 	const { isUploading, uploadErrors, handleFileChange, resetUpload } =
 		useScriptsUpload();
 
-	const [isLoadingMore, setIsLoadingMore] = useState(false);
-
-	const allCurrentIds = useMemo(
-		() => [...folders.map((f) => f.id), ...scripts.map((s) => s.id)],
-		[folders, scripts],
-	);
+	const {
+		allCurrentIds,
+		isLoadingMore,
+		isRoot,
+		isEmpty,
+		currentFolderName,
+		handleNavigate,
+		handleLoadMore,
+	} = useScriptsPageController();
 
 	useScriptsEvents(allCurrentIds);
-
-	const [
-		createFolderOpened,
-		{ open: openCreateFolder, close: closeCreateFolder },
-	] = useDisclosure(false);
-	const [moveModalOpened, { open: openMoveModal, close: closeMoveModal }] =
-		useDisclosure(false);
-	const [
-		deleteItemsOpened,
-		{ open: openDeleteItems, close: closeDeleteItems },
-	] = useDisclosure(false);
-
-	const [deleteFolderTarget, setDeleteFolderTarget] = useState<Folder | null>(
-		null,
-	);
-	const [deleteScriptTarget, setDeleteScriptTarget] =
-		useState<ScriptSummary | null>(null);
-	const [sortAscending, setSortAscending] = useState(true);
 
 	useEffect(() => {
 		init();
 	}, [init]);
-
-	const handleNavigate = async (folderId: string | null) => {
-		setCurrentFolder(folderId);
-		await fetchFolderData(folderId);
-	};
-
-	const handleLoadMore = async () => {
-		setIsLoadingMore(true);
-		try {
-			await loadMoreScripts(currentFolderId);
-		} finally {
-			setIsLoadingMore(false);
-		}
-	};
-
-	const isRoot = useMemo(() => currentFolderId === null, [currentFolderId]);
-	const isEmpty = useMemo(
-		() => folders.length === 0 && scripts.length === 0,
-		[folders.length, scripts.length],
-	);
-	const currentFolderName = useMemo(
-		() =>
-			breadcrumb.length > 0
-				? breadcrumb[breadcrumb.length - 1].name
-				: undefined,
-		[breadcrumb],
-	);
 
 	return (
 		<Flex direction="column" h="100%" rowGap="md">
@@ -136,7 +70,7 @@ export default function ScriptsPage() {
 				onResetUpload={resetUpload}
 				viewMode={viewMode}
 				onViewModeChange={setViewMode}
-				onCreateFolder={openCreateFolder}
+				onCreateFolder={() => setCreateFolderOpened(true)}
 				onUpload={handleFileChange}
 				isUploading={isUploading}
 			/>
@@ -155,7 +89,7 @@ export default function ScriptsPage() {
 				<Box flex={1} px="md">
 					<ScriptsEmptyState
 						isRoot={isRoot}
-						onCreateFolder={openCreateFolder}
+						onCreateFolder={() => setCreateFolderOpened(true)}
 						onUpload={handleFileChange}
 						isUploading={isUploading}
 					/>
@@ -164,24 +98,11 @@ export default function ScriptsPage() {
 				<Flex flex={1} mih={0}>
 					<Box flex={1} p="md" miw={0} style={{ overflowY: "auto" }}>
 						<ScriptsLibraryItems
-							viewMode={viewMode}
-							folders={folders}
-							scripts={scripts}
-							folderItemCounts={folderChildItemCounts}
-							sortAscending={sortAscending}
-							onSortAscendingChange={setSortAscending}
-							selectedScriptId={selectedScript?.id ?? null}
-							selectedIds={selectedIds}
 							onNavigateFolder={handleNavigate}
-							onDeleteFolder={(folder) => setDeleteFolderTarget(folder)}
-							onSelectScript={selectScript}
-							onDeleteScript={(script) => setDeleteScriptTarget(script)}
-							onToggleSelection={(id, isMulti, isRange) =>
-								toggleSelection(id, isMulti, isRange, allCurrentIds)
-							}
 							hasMoreScripts={hasMoreScripts}
 							isLoadingMore={isLoadingMore}
 							onLoadMore={handleLoadMore}
+							allCurrentIds={allCurrentIds}
 						/>
 					</Box>
 
@@ -196,8 +117,8 @@ export default function ScriptsPage() {
 					<MultiSelectToolbar
 						selectedCount={selectedIds.length}
 						onClear={clearSelection}
-						onDelete={openDeleteItems}
-						onMove={openMoveModal}
+						onDelete={() => setDeleteItemsOpened(true)}
+						onMove={() => setMoveModalOpened(true)}
 						onCopy={async () => {
 							const scriptIds = selectedIds.filter((id) =>
 								scripts.some((s) => s.id === id),
@@ -209,19 +130,7 @@ export default function ScriptsPage() {
 				</Flex>
 			)}
 
-			<ScriptsModals
-				deleteFolderTarget={deleteFolderTarget}
-				setDeleteFolderTarget={setDeleteFolderTarget}
-				deleteScriptTarget={deleteScriptTarget}
-				setDeleteScriptTarget={setDeleteScriptTarget}
-				createFolderOpened={createFolderOpened}
-				closeCreateFolder={closeCreateFolder}
-				moveModalOpened={moveModalOpened}
-				closeMoveModal={closeMoveModal}
-				deleteItemsOpened={deleteItemsOpened}
-				closeDeleteItems={closeDeleteItems}
-				currentFolderName={currentFolderName}
-			/>
+			<ScriptsModals currentFolderName={currentFolderName} />
 		</Flex>
 	);
 }
